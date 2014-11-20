373d62aeaa2397dfcadb1e758436f7369a328e29
ranocohen <ranocohen@gmail.com>
2014-11-20 10:49:46 +0200

    vcs

--- wear/src/main/java/app/wearable/gdg/com/gdgpitcher/MainActivity.java
+++ wear/src/main/java/app/wearable/gdg/com/gdgpitcher/MainActivity.java
@@ -6,7 +6,6 @@ import android.support.wearable.view.WatchViewStub;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
-    //TEST
     private TextView mTextView;
 
     @Override
